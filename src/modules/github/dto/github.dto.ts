e9c91cc5bf155096ff78677d6a1b7c4c4b2c9648
Daniel <daniel.cn94@gmail.com>
2021-08-22 17:28:55 -0500

    Implement API documentation with Swagger
    - Add @nestjs/swagger & swagger-ui-express modules to package.json
    - Add documentation to GetCommits and GetBranches endpoints
    - Update root endpoint to be SwaggerUI
    - Added /healthcheck endpoint to replace previos root endpoint

--- src/modules/github/dto/github.dto.ts
+++ src/modules/github/dto/github.dto.ts
@@ -1,10 +1,20 @@
+import { ApiResponseProperty } from '@nestjs/swagger';
+
 export class FindBranchesResponseDto {
+  @ApiResponseProperty({ example: 'develop' })
   name: string;
 }
 
 export class FindCommitsResponseDto {
+  @ApiResponseProperty({ example: 'Daniel' })
   author: string;
+
+  @ApiResponseProperty({ example: '2021-08-21T22:41:51Z' })
   date: string;
+
+  @ApiResponseProperty({ example: 'Lorem Ipsum' })
   message: string;
+
+  @ApiResponseProperty({ example: '6b16637841d2228b970e90975eef4a4aa7185dd7' })
   sha: string;
 }
--- src/modules/github/github.controller.ts
+++ src/modules/github/github.controller.ts
@@ -1,5 +1,12 @@
 import { Controller, Get, Param, Query } from '@nestjs/common';
 import {
+  ApiOkResponse,
+  ApiNotFoundResponse,
+  ApiQuery,
+  ApiOperation,
+  ApiParam,
+} from '@nestjs/swagger';
+import {
   FindBranchesResponseDto,
   FindCommitsResponseDto,
 } from './dto/github.dto';
@@ -10,14 +17,37 @@ export class GithubController {
   constructor(private readonly githubService: GithubService) {}
 
   @Get('/commits')
+  @ApiOperation({ summary: 'Get Commits by Repository' })
+  @ApiParam({
+    name: 'repositoryName',
+    example: 'daniel-hcn/github-commit-history-server',
+  })
+  @ApiQuery({ name: 'branchName', required: false, example: 'develop' })
+  @ApiOkResponse({
+    description: 'List of Commits found',
+    type: FindCommitsResponseDto,
+    isArray: true,
+  })
+  @ApiNotFoundResponse({ description: 'Repository not found' })
   async getCommits(
     @Param('repositoryName') repositoryName: string,
-    @Query('branch') branchName?: string,
+    @Query('branchName') branchName?: string,
   ): Promise<FindCommitsResponseDto[]> {
     return await this.githubService.getCommits(repositoryName, branchName);
   }
 
   @Get('/branches')
+  @ApiOperation({ summary: 'Get Branches by Repository' })
+  @ApiParam({
+    name: 'repositoryName',
+    example: 'daniel-hcn/github-commit-history-server',
+  })
+  @ApiOkResponse({
+    description: 'List of Branches found',
+    type: FindBranchesResponseDto,
+    isArray: true,
+  })
+  @ApiNotFoundResponse({ description: 'Repository not found' })
   async getBranches(
     @Param('repositoryName') repositoryName: string,
   ): Promise<FindBranchesResponseDto[]> {
